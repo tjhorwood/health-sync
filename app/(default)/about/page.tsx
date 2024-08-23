16e7e82a2ab90a58d67ddbcdb3cc7606ef545f48
Taylor Horwood <taylor.horwood@outlook.com>
2024-08-23 16:17:01 +0000

    updated contact page

--- app/(default)/about/page.tsx
+++ app/(default)/about/page.tsx
@@ -1,8 +1,3 @@
-export const metadata = {
-  title: 'About - Simple',
-  description: 'Page description',
-};
-
 import Intro from './intro';
 import Story from './story';
 import Stats from '@/components/stats';
@@ -10,6 +5,11 @@ import Team from '@/components/team';
 import Career from '@/components/career';
 import Process from '@/components/process';
 import Cta from '@/components/cta-02';
+
+export const metadata = {
+  title: 'About - Health Sync',
+  description: 'About us',
+};
 
 export default function About() {
   return (
---
+++ components/ui/input.tsx
@@ -0,0 +1,56 @@
+// Input component extends from shadcnui - https://ui.shadcn.com/docs/components/input
+"use client";
+import * as React from "react";
+import { cn } from "@/lib/utils";
+import { useMotionTemplate, useMotionValue, motion } from "framer-motion";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    const radius = 100; // change this to increase the rdaius of the hover effect
+    const [visible, setVisible] = React.useState(false);
+
+    let mouseX = useMotionValue(0);
+    let mouseY = useMotionValue(0);
+
+    function handleMouseMove({ currentTarget, clientX, clientY }: any) {
+      let { left, top } = currentTarget.getBoundingClientRect();
+
+      mouseX.set(clientX - left);
+      mouseY.set(clientY - top);
+    }
+    return (
+      <motion.div
+        style={{
+          background: useMotionTemplate`
+        radial-gradient(
+          ${visible ? radius + "px" : "0px"} circle at ${mouseX}px ${mouseY}px,
+          var(--blue-500),
+          var(--teal-400), 
+          transparent 80%
+        )
+      `,
+        }}
+        onMouseMove={handleMouseMove}
+        onMouseEnter={() => setVisible(true)}
+        onMouseLeave={() => setVisible(false)}
+        className="p-[2px] rounded-lg transition duration-300 group/input"
+      >
+        <input
+          type={type}
+          className={cn(
+            `flex h-12 w-full border bg-white text-black shadow-input rounded-sm px-3 py-2 placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-blue-500 disabled:cursor-not-allowed disabled:opacity-50 group-hover/input:shadow-none transition duration-400`,
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+      </motion.div>
+    );
+  }
+);
+Input.displayName = "Input";
+
+export { Input };
