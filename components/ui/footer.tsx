14332426f6fd9f1f7b0256394c9850e8fd6b8f32
Taylor Horwood <taylor.horwood@outlook.com>
2024-08-23 17:14:55 +0000

    updated footer

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -3,7 +3,7 @@ import Laptop from '@/public/images/laptop.webp';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from './button';
-import { FaApple, FaWindows } from 'react-icons/fa';
+import { FaApple, FaFacebook, FaInstagram, FaTwitter, FaWindows } from 'react-icons/fa';
 
 export default function Footer() {
   return (
@@ -106,50 +106,32 @@ export default function Footer() {
         {/* Bottom area */}
         <div className='md:flex md:items-center md:justify-between py-4 md:py-8 border-t border-gray-200'>
           {/* Social as */}
-          <ul className='flex mb-4 md:order-1 md:ml-4 md:mb-0'>
+          <ul className='flex mb-4 md:order-1 md:ml-4 md:mb-0 space-x-4'>
+            <li>
+              <a
+                href='#0'
+                className='flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out'
+                aria-label='Facebook'
+              >
+                <FaFacebook className='h-9 w-9 fill-current p-2' />
+              </a>
+            </li>
+            <li>
+              <a
+                href='#0'
+                className='flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out'
+                aria-label='Instagram'
+              >
+                <FaInstagram className='h-9 w-9 fill-current p-2' />
+              </a>
+            </li>
             <li>
               <a
                 href='#0'
                 className='flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out'
                 aria-label='Twitter'
               >
-                <svg
-                  className='w-8 h-8 fill-current'
-                  viewBox='0 0 32 32'
-                  xmlns='http://www.w3.org/2000/svg'
-                >
-                  <path d='m13.063 9 3.495 4.475L20.601 9h2.454l-5.359 5.931L24 23h-4.938l-3.866-4.893L10.771 23H8.316l5.735-6.342L8 9h5.063Zm-.74 1.347h-1.457l8.875 11.232h1.36l-8.778-11.232Z' />
-                </svg>
-              </a>
-            </li>
-            <li className='ml-4'>
-              <a
-                href='#0'
-                className='flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out'
-                aria-label='Github'
-              >
-                <svg
-                  className='w-8 h-8 fill-current'
-                  viewBox='0 0 32 32'
-                  xmlns='http://www.w3.org/2000/svg'
-                >
-                  <path d='M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z' />
-                </svg>
-              </a>
-            </li>
-            <li className='ml-4'>
-              <a
-                href='#0'
-                className='flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out'
-                aria-label='Facebook'
-              >
-                <svg
-                  className='w-8 h-8 fill-current'
-                  viewBox='0 0 32 32'
-                  xmlns='http://www.w3.org/2000/svg'
-                >
-                  <path d='M14.023 24L14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257z' />
-                </svg>
+                <FaTwitter className='h-9 w-9 fill-current p-2' />
               </a>
             </li>
           </ul>
